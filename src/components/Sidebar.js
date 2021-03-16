852c65bbcda61189c38efaf507cc5a1ca2c268f1
iamtamtamiam <vutam_a@hotmail.com>
2021-03-16 10:40:41 -0500

    add and style new message button

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 function Sidebar(){
     //3 sections: workspace, threads, channels
@@ -14,7 +15,7 @@ function Sidebar(){
                     IamTam
                 </Name>
                 <NewMessage>
-
+                    <AddCircleOutlineIcon/>
                 </NewMessage>
 
             </WorkspaceContainer>
@@ -35,6 +36,11 @@ const Container = styled.div`
 const WorkspaceContainer = styled.div`
     color:white; 
     height: 64px;
+    display: flex;
+    align-items: center;
+    padding-left: 19px;
+    justify-content: space-between;
+    border-bottom: 1px solid #532753;
 `
 
 const Name = styled.div`
@@ -42,5 +48,15 @@ const Name = styled.div`
 `
 
 const NewMessage = styled.div`
-    
+    width: 36px;
+    height: 36px;
+    background: white;
+    color: #3F0E40;
+    fill: #3F0E40;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right:20px;
+    cursor:pointer;
 `
